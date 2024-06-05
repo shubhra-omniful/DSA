e69a1ea62709cbfddb792f94092b1251e426fcaa
Shubhra Kanti Ghosh <shubhrakantighosh@Shubhras-MacBook-Pro.local>
2024-06-05 23:44:38 +0530

    day 3

---
+++ factorial2.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.*;
+
+public class factorial2{
+
+	public static int factorial(int num){
+		if (num ==0 || num ==1){
+			return 1;
+		}
+
+		return num*factorial(num-1);
+	}
+
+	public static void main(String[] args){
+		System.out.println(factorial(5));
+	}
+}
